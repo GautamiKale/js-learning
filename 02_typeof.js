96d5e7fc29dfc0273d05906c0dd93939df33161d
GautamiKale <gautamisaurabh@gmail.com>
2024-03-10 00:31:22 +0530

    Update 02_typeof.js

--- 02_typeof.js
+++ 02_typeof.js
@@ -1,17 +1,46 @@
+console.log("=============step1============");
+console.log("-------Before Initailization-------");
 
 var bankName; 
 console.log("bankName;:", bankName);
 var typeOfbankName = typeof bankName;
 console.log("Type of bankName:", bankName);
 
+console.log("-------After Initailization-------");
+
 var bankName;
 bankName="SBI Bank";
 console.log("bankName value",bankName);
 var result=typeof bankName;
 console.log("Type of",result);
-
-
+console.log("============step2==============");
 var marks="90%";
-console.log("marks",marks);
-var result=typeof marks;
-console.log("Type of",result);
+console.log("Marks:", marks);
+
+var typeofMarks = typeof marks;
+console.log("Type of Marks is:", typeofMarks);
+
+
+console.log("============step3==============");
+const isWorking = true;
+console.log(Boolean(isWorking));
+console.log(typeof Boolean(isWorking));
+
+console.log("============step4==============");
+console.log("-------Before Initailization-------");
+var totalcount; // Declare
+console.log("Totalcount", totalcount);
+var typeOfTotalcount = typeof totalcount;
+console.log("Type of totalcount", typeOfTotalcount);
+
+console.log("-------After Initailization-------");
+var totalcount="Hundred and seven";
+console.log("totalcount:", totalcount);
+var typeoftotalcount= typeof totalcount;
+console.log("Type of totalcount is:", typeoftotalcount);
+ totalcount=107;
+ console.log("totalcount:", totalcount);
+ var typeoftotalcount= typeof totalcount;
+ console.log("Type of totalcount is:", typeoftotalcount);
+
+
